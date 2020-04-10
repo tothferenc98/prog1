f30d071ffba282343a17e36f5fc4a112aecc7e3f
tothferenc98 <55550399+tothferenc98@users.noreply.github.com>
2020-04-10 21:39:02 +0200

    Polargen - 6.1. Első osztályom

---
+++ polargen.h
@@ -0,0 +1,27 @@
+#ifndef POLARGEN__H
+#define POLARGEN__H
+
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+
+class PolarGen
+{
+public:
+  PolarGen ()
+  {
+    nincsTarolt = true;
+    std::srand (std::time (NULL));
+  }
+   ~PolarGen ()
+  {
+  }
+  double kovetkezo ();
+
+private:
+  bool nincsTarolt;
+  double tarolt;
+
+};
+
+#endif
